640265c63153b5ec0ad415dd672c70119279da40
Piyawat10070400 <piyawat5930302600@gmail.com>
2026-02-01 10:00:41 +0700

    update route

--- routes/action.route.js
+++ routes/action.route.js
@@ -12,15 +12,15 @@ import {
 } from "../controllers/action.controller.js";
 
 const router = express.Router();
+router.get("/notification/today", checkAndNotifyDailyActions);
+router.get("/notification/upcoming", checkAndNotifyUpcomingActions);
+router.post("/test-email", testMail);
+router.get("/current", getCurrentActions);
 
 router.get("/", getActions);
 router.get("/:id", getActionById);
 router.post("/", createAction);
 router.put("/:id", updateAction);
 router.delete("/:id", deleteAction);
-router.get("/notification/today", checkAndNotifyDailyActions);
-router.get("/notification/upcoming", checkAndNotifyUpcomingActions);
-router.post("/test-email", testMail);
-router.get("/current", getCurrentActions);
 
 export default router;
